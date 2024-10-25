0968a95bcd291f169c8d620ac331c7b9e6fcc856
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-25 23:36:05 +0530

    All setup done

--- Startingwithserver/index.js
+++ Startingwithserver/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
 const app = express();
+
+app.get("/home",(req,res)=>
+{
+    res.send("Home Page")
+});
 const Port =  5005;
 app.listen(Port,()=>
 {
-    console.log(`server is running${Port}`);
+    console.log(`server is running on port : ${Port}`);
 })
