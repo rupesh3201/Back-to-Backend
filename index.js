66f36665c64f978b4b29fe9fe0fb448b5406b1a3
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-25 23:53:37 +0530

    // if we do any cahnges are not refected directly soo we need to find soultuion we need to restart the server

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get("/home",(req,res)=>
     res.send("Home Page is here");
     // api  hit sucessfully 
 })
+app.get("/contact",(req,res)=>
+{
+        res.send("Contact Page is here");
+        // if we do any cahnges are not refected directly soo we need to find soultuion we need to restart the server
+        
+})
 
 const PORT= 4000;
 console.log(`Server is Running on Port Number ${PORT}`);
