ad1e09c8616a845c8e819e33065ce431b902cd45
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-26 00:19:21 +0530

    added data in json format

--- index.js
+++ index.js
@@ -17,7 +17,19 @@ app.get("/contact",(req,res)=>
         // if we do any cahnges are not refected directly soo we need to find soultuion we need to restart the server
 
 })
-
+// sending json 
+app.get("/data",(req,res)=>
+    {
+        res.json(
+            {
+                sucess : true,
+                msg : 'Pass',
+                marks : 90 ,
+                subject : 'hindi'
+            }
+        )
+        // api  hit sucessfully 
+    })
 const PORT= 4000;
 console.log(`Server is Running on Port Number ${PORT}`);
 app.listen(4000);
