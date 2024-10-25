588fe697340ea67ed14d8b08eea3ea226bd7a9cf
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-25 23:09:20 +0530

    created basic server

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express  from 'express';
-
 const server = express();
 // get request 
 // (req,res) are obj
-
 server.get("/hi",(req,res)=>{
     res.send("hellow");
 });
 server.get("/ping",(req,res)=>{
     res.send("pong");
 });
+// created a basic server by using express 
 server.listen(5004);
